adb1576aac9603640fa5e464ad1798d80e2cbaf0
Bianca Power <drbiancampower@gmail.com>
2017-12-08 06:57:19 +1100

    added express-handlebars; added view (including a layouts/main) and rendered them

--- app.js
+++ app.js
@@ -1,15 +1,25 @@
 const express = require('express');
+const exphbs  = require('express-handlebars');
 
 const app = express();
 
+// Handlebars Middleware
+app.engine('handlebars', exphbs({
+  defaultLayout: 'main'
+}));
+app.set('view engine', 'handlebars');
+
 // Index Route
 app.get('/', (req, res) => {
-  res.send('INDEX');
+  const title = "Hey there ideasmaster!"
+  res.render('index', {
+    title: title
+  });
 });
 
 // About Route
 app.get('/about', (req, res) => {
-  res.send('ABOUT');
+  res.render('about');
 })
 
 const port = 5000;
