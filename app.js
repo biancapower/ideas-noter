8e132bb34d7cee833253a0b6f102acf56203e8c5
Bianca Power <drbiancampower@gmail.com>
2017-12-08 17:18:50 +1100

    added server-side validation

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const exphbs  = require('express-handlebars');
+const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 
 const app = express();
@@ -24,6 +25,10 @@ app.engine('handlebars', exphbs({
 }));
 app.set('view engine', 'handlebars');
 
+// Body Parser Middleware
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json())
+
 // Index Route
 app.get('/', (req, res) => {
   const title = "Ideas Noter"
@@ -35,12 +40,37 @@ app.get('/', (req, res) => {
 // About Route
 app.get('/about', (req, res) => {
   res.render('about');
-})
+});
 
 // Add Idea Form
 app.get('/ideas/add', (req, res) => {
   res.render('ideas/add');
-})
+});
+
+// Process Idea Form
+app.post('/ideas', (req, res) => {
+
+  // server-side validation
+  let errors = [];
+
+  if(!req.body.title){
+    errors.push({text: 'Please add a title'});
+  }
+
+  if(!req.body.details){
+    errors.push({text: 'Please add some details'});
+  }
+
+  if(errors.length > 0){
+    res.render('ideas/add', {
+      errors: errors,
+      title: req.body.title,
+      details: req.body.details
+    });
+  } else {
+    res.send('passed');
+  }
+});
 
 const port = 5000;
 
