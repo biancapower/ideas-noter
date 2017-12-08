93cf6b28884668eb4f09e237c2c91de8e9337f80
Bianca Power <drbiancampower@gmail.com>
2017-12-08 14:18:08 +1100

    added idea model

--- app.js
+++ app.js
@@ -14,6 +14,10 @@ mongoose.connect('mongodb://localhost/ideasnoter-dev', {
   .then(() => console.log('MongoDB Connected...'))
   .catch(err => console.log(err));
 
+// Load Idea Model
+require('./models/Idea');
+const Idea = mongoose.model('ideas');
+
 // Handlebars Middleware
 app.engine('handlebars', exphbs({
   defaultLayout: 'main'
