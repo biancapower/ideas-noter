a58ce085274393e75f54d9adc6c01dbfc2edbff0
Bianca Power <drbiancampower@gmail.com>
2017-12-09 21:36:57 +1100

    signup form validation

--- routes/users.js
+++ routes/users.js
@@ -12,4 +12,29 @@ router.get('/signup', (req, res) => {
   res.render('users/signup');
 });
 
+// Signup Form POST
+router.post('/signup', (req, res) => {
+  let errors = [];
+
+  if(req.body.password != req.body.password2) {
+    errors.push({text: 'Passwords don\'t match'});
+  }
+
+  if(req.body.password.length < 4) {
+    errors.push({text: 'Passwords must be at least 4 characters'});
+  }
+
+  if(errors.length > 0) {
+    res.render('users/signup', {
+      errors: errors,
+      name: req.body.name,
+      email: req.body.email,
+      password: req.body.password,
+      password2: req.body.password2
+    })
+  } else {
+    res.send('passed');
+  }
+});
+
 module.exports = router;
